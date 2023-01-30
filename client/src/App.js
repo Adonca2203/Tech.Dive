09d26f4920ffba63a63e7d4b32706e647850a8be
Adonis Carvajal <adonis.carvajal2203@gmail.com>
2023-01-29 21:01:29 -0500

    Fixed leftover response

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <MainPage response={response} />
+                <MainPage />
             </header>
         </div>
     );
