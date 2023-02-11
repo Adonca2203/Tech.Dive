d15ad6fc73183eebb8badf9b6f5ea3ba5ff61f60
Waliyat1 <53194072+Waliyat1@users.noreply.github.com>
2023-02-11 18:06:24 -0500

    Changes

--- client/src/subComponent/ExamDetails.js
+++ client/src/subComponent/ExamDetails.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 
 const ExamDetails = () => {
-  return (
-    <div><h3>Exam Details</h3></div>
-  )
+  const { id } = useParams();
+  const [exam, setExam] = useState({});
+
+  useEffect(() => {
+    fetch(`/api/exams/${id}`)
+      .then(res => res.json())
+      .then(data => setExam(data));
+  }, [id]);
+
+    return (
+        <div>
+          <h3>Exam Details</h3>
+          <p>Patient ID: {exam.patientId}</p>
+          <p>Exam ID: {exam.examId}</p>
+          <p>Key findings: {exam.keyFindings}</p>
+          <p>Brixia score: {exam.brixiaScore}</p>
+          <p>Age: {exam.age}</p>
+          <p>Sex: {exam.sex}</p>
+          <p>BMI: {exam.bmi}</p>
+          <p>Zip code: {exam.zipCode}</p>
+          <img src={exam.imageUrl} alt="Patient's Chest X-Ray" />  
+        </div>
+    );
 }
 
 export default ExamDetails;
+
+
+
+
