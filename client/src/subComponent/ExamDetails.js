206fed010ce14329d6186e635a86ede05eabc2e7
Kai Brown <104911237+kaibrown28@users.noreply.github.com>
2023-02-13 18:45:10 -0500

    trying to resolve a local merge issue

--- client/src/subComponent/ExamDetails.js
+++ client/src/subComponent/ExamDetails.js
@@ -1,23 +1,17 @@
-//NOTES - persistent error ERROR in ./src/data/index.js 5:0-29
-
-// export 'default' (reexported as 'Columns') was not found in './columns' (possible exports: Columns)
-
-//I'm commenting out the table I'm trying to use in order to get a dummy table on the screen
-
-
 // import { React, useMemo } from 'react'
 // import { useTable } from 'react-table'
 // import { Columns } from '../data/columns'
 // import examData from '../data/data.json'
 // import { Search } from '../subComponent';
 
-const ExamDetails = () => {
+import { useApi } from '../hooks/use-api';
+
+const ExamDetails = (props) => {
+
 // const columns = useMemo(() => Columns, [])
 // const ExamData = useMemo(() => examData, [])
 
 // const tableInstance = useTable({})
-
-
 //   const {
 //    getTableProps,
 //    getTableBodyProps,
@@ -25,55 +19,23 @@ const ExamDetails = () => {
 //    rows,
 //    prepareRow,
 //   } = tableInstance    
-  
-  return (
-    <>
-      <div>
-        <h3>Exam Details</h3>
-        {/* <Search /> */}
-        </div>
+ 
 
-      <container>
-        {/* <table {...getTableProps()}>
-          <thead>
-            {headerGroups.map(headerGroup => (
-              <tr {...headerGroup.getHeaderGroupProps()}>
-                {
-                  headerGroup.headers.map(column => (
-                    <th {...column.getHeaderProps()}>
-                      {column.render('Header')}
-                    </th>
-                  ))}
-              </tr>
-            ))}
-          </thead> */}
-          <table>
-            <thead>
-              <tr>
-                <th>Patient ID</th>
-                <th>Exam ID</th>
-                <th>Image</th>
-                <th>Key Findings</th>
-                <th>Brixia Score</th>
-                <th>Age</th>
-                <th>Sex</th>
-                <th>Zip Code</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>John Doe</td>
-                <td>30</td>
-                <td>New York</td>
-              </tr>
-              <tr>
-                <td>Jane Doe</td>
-                <td>25</td>
-                <td>London</td>
-              </tr>
-            </tbody>
-          </table>
-      </container>
+const testID = "63ddab3dc9c1d0397e4c7b08";
+const { response } = useApi({ path: `exams/${props.id}` }); 
+
+if (response) {
+  return(
+    // table
+    <tr>
+      
+    </tr>
+  );
+}
+
+return (
+    <>
+   
     </>
   );
 }
