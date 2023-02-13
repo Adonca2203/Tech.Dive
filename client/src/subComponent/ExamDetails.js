b9084dd3faf8983b96c27deb32271c425b1184a9
Brown <KBrown67@its.jnj.com>
2023-02-13 15:09:34 -0500

    applied react-bootstrap cards to examdetails page

--- client/src/subComponent/ExamDetails.js
+++ client/src/subComponent/ExamDetails.js
@@ -1,84 +1,85 @@
-//NOTES - persistent error ERROR in ./src/data/index.js 5:0-29
-
-// export 'default' (reexported as 'Columns') was not found in './columns' (possible exports: Columns)
-
-//I'm commenting out the table I'm trying to use in order to get a dummy table on the screen
-
-
 // import { React, useMemo } from 'react'
 // import { useTable } from 'react-table'
 // import { Columns } from '../data/columns'
 // import examData from '../data/data.json'
-// import { Search } from '../subComponent';
+import { Search } from "../subComponent";
+import { Card, Container } from "react-bootstrap";
 
 const ExamDetails = () => {
-// const columns = useMemo(() => Columns, [])
-// const ExamData = useMemo(() => examData, [])
-
-// const tableInstance = useTable({})
-
-
-//   const {
-//    getTableProps,
-//    getTableBodyProps,
-//    headerGroups,
-//    rows,
-//    prepareRow,
-//   } = tableInstance    
-  
   return (
     <>
       <div>
         <h3>Exam Details</h3>
-        {/* <Search /> */}
-        </div>
+        <Search />
+      </div>
 
-      <container>
-        {/* <table {...getTableProps()}>
-          <thead>
-            {headerGroups.map(headerGroup => (
-              <tr {...headerGroup.getHeaderGroupProps()}>
-                {
-                  headerGroup.headers.map(column => (
-                    <th {...column.getHeaderProps()}>
-                      {column.render('Header')}
-                    </th>
-                  ))}
-              </tr>
-            ))}
-          </thead> */}
-          <table>
-            <thead>
-              <tr>
-                <th>Patient ID</th>
-                <th>Exam ID</th>
-                <th>Image</th>
-                <th>Key Findings</th>
-                <th>Brixia Score</th>
-                <th>Age</th>
-                <th>Sex</th>
-                <th>Zip Code</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>John Doe</td>
-                <td>30</td>
-                <td>New York</td>
-              </tr>
-              <tr>
-                <td>Jane Doe</td>
-                <td>25</td>
-                <td>London</td>
-              </tr>
-            </tbody>
-          </table>
-      </container>
+      <Container className="PatientInfo">
+        <Card className="PatientID">
+          <Card.Body>
+            <Card.Title>Patient ID</Card.Title>
+            <Card.Text>#</Card.Text>
+          </Card.Body>
+        </Card>
+        <Card className="Age">
+          <Card.Body>
+            <Card.Title>Age</Card.Title>
+            <Card.Text># </Card.Text>
+          </Card.Body>
+        </Card>
+        <Card className="Sex">
+          <Card.Body>
+            <Card.Title>Sex</Card.Title>
+            <Card.Text>#</Card.Text>
+          </Card.Body>
+        </Card>
+        <Card className="BMI">
+          <Card.Body>
+            <Card.Title>BMI</Card.Title>
+            <Card.Text>#</Card.Text>
+          </Card.Body>
+        </Card>
+        <Card className="ZipCode">
+          <Card.Body>
+            <Card.Title>Zip Code</Card.Title>
+            <Card.Text>#</Card.Text>
+          </Card.Body>
+        </Card>
+      </Container>
+      <Container className="ExamInfo">
+        <Card className="ExamID">
+          <Card.Body>
+            <Card.Title>Exam ID</Card.Title>
+            <Card.Text>#</Card.Text>
+          </Card.Body>
+        </Card>
+        <Card className="Image">
+          <Card.Body>
+            <Card.Title>Image URL</Card.Title>
+            <Card.Text>URL:</Card.Text>
+            <Card.Image>Image:</Card.Image>
+          </Card.Body>
+        </Card>
+        <Card className="Date">
+          <Card.Body>
+            <Card.Title>Date</Card.Title>
+            <Card.Text>#</Card.Text>
+          </Card.Body>
+        </Card>
+        <Card className="KeyFindings">
+          <Card.Body>
+            <Card.Title>Key Findings</Card.Title>
+            <Card.Text>#</Card.Text>
+          </Card.Body>
+        </Card>
+        <Card className="BrixiaScore">
+          <Card.Body>
+            <Card.Title>Brixia Score (seperated by Comma)</Card.Title>
+            <Card.Text>#</Card.Text>
+          </Card.Body>
+        </Card>
+      </Container>
     </>
   );
-}
+};
 
-
-  
-  export default ExamDetails;
-  
+export default ExamDetails;
