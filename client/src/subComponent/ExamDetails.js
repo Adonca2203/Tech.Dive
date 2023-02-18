350d023f9f5eb89c5fcf716946c77bf9c055de46
Kai Brown <104911237+kaibrown28@users.noreply.github.com>
2023-02-17 19:36:52 -0500

    set up axios to fetch data

--- client/src/subComponent/ExamDetails.js
+++ client/src/subComponent/ExamDetails.js
@@ -1,44 +1,35 @@
-// import { React, useMemo } from 'react'
-// import { useTable } from 'react-table'
-// import { Columns } from '../data/columns'
-// import examData from '../data/data.json'
 import { Search } from "../subComponent";
 import { Card, Container } from "react-bootstrap";
+import axios from 'axios'
+import { useEffect, useState } from "react";
 
-// import { useApi } from '../hooks/use-api';
+import { useApi } from '../hooks/use-api';
 
-// const ExamDetails = (props) => {
 
-// const columns = useMemo(() => Columns, [])
-// const ExamData = useMemo(() => examData, [])
 
-// const tableInstance = useTable({})
-//   const {
-//    getTableProps,
-//    getTableBodyProps,
-//    headerGroups,
-//    rows,
-//    prepareRow,
-//   } = tableInstance    
- 
 
-// const testID = "63ddab3dc9c1d0397e4c7b08";
-// const { response } = useApi({ path: `exams/${props.id}` }); 
-
-// if (response) {
-//   return(
-//     // table
-//     <tr>
-      
-//     </tr>
-//   );
-// }
-
-// return (
-//     <>
    
-const ExamDetails = () => {
+const ExamDetails = (props) => {
+
+  let [exam, setExam] = useState([])
+  // const testID = "63ddab3dc9c1d0397e4c7b08";
+  const { response } = useApi({ path: `exams/${props.id}`})
+  
+  const getExam = () => {
+    axios
+      .get(response)
+      .then(
+        (res => setExam(res.data),
+        (err) => console.error(err)
+      )
+      .catch((error) => console.error))
+  };
+  
+  useEffect(() => {
+    getExam()
+  }, [])
   return (
+   
     <>
       <div>
         <h3>Exam Details</h3>
@@ -46,34 +37,34 @@ const ExamDetails = () => {
       </div>
 
       <Container className="PatientInfo">
-        <Card className="PatientID">
+        <Card className="PatientID" key={exam.id}>
           <Card.Body>
             <Card.Title>Patient ID</Card.Title>
-            <Card.Text>#</Card.Text>
+            <Card.Text>{exam.patientId}</Card.Text>
           </Card.Body>
         </Card>
         <Card className="Age">
           <Card.Body>
             <Card.Title>Age</Card.Title>
-            <Card.Text># </Card.Text>
+            <Card.Text>{exam.age}</Card.Text>
           </Card.Body>
         </Card>
         <Card className="Sex">
           <Card.Body>
             <Card.Title>Sex</Card.Title>
-            <Card.Text>#</Card.Text>
+            <Card.Text>{exam.sex}</Card.Text>
           </Card.Body>
         </Card>
         <Card className="BMI">
           <Card.Body>
             <Card.Title>BMI</Card.Title>
-            <Card.Text>#</Card.Text>
+            <Card.Text>{exam.bmi}</Card.Text>
           </Card.Body>
         </Card>
         <Card className="ZipCode">
           <Card.Body>
             <Card.Title>Zip Code</Card.Title>
-            <Card.Text>#</Card.Text>
+            <Card.Text>{exam.zipCode}</Card.Text>
           </Card.Body>
         </Card>
       </Container>
@@ -81,7 +72,7 @@ const ExamDetails = () => {
         <Card className="ExamID">
           <Card.Body>
             <Card.Title>Exam ID</Card.Title>
-            <Card.Text>#</Card.Text>
+            <Card.Text>{exam.examId}</Card.Text>
           </Card.Body>
         </Card>
         <Card className="Image">
@@ -100,13 +91,13 @@ const ExamDetails = () => {
         <Card className="KeyFindings">
           <Card.Body>
             <Card.Title>Key Findings</Card.Title>
-            <Card.Text>#</Card.Text>
+            <Card.Text>{exam.keyFindings}</Card.Text>
           </Card.Body>
         </Card>
         <Card className="BrixiaScore">
           <Card.Body>
             <Card.Title>Brixia Score (seperated by Comma)</Card.Title>
-            <Card.Text>#</Card.Text>
+            <Card.Text>{exam.brixiaScore}</Card.Text>
           </Card.Body>
         </Card>
       </Container>
