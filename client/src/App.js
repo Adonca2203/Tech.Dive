4920dd0dd28f6bf2e7f7ff858cf063e4727067cb
DDjohnson21 <Damienjking84@gmail.com>
2023-02-18 21:17:05 -0500

    Damien updated code for exam pages creating puttons and upload photos

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,10 @@
 import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from "react-router-dom";
-
+// Import necessary libraries and components
 import './App.css';
-
 import { Admin, Exams, HeaderCom } from './components';
 import { useApi } from './hooks/use-api';
 
-
+// Create the router using react-router-dom
 const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path='/' element={<div><HeaderCom /> <Exams /></div>}>
@@ -14,7 +13,9 @@ const router = createBrowserRouter(
     )
 )
 
+// This component is used to render a list of exams
 const ExamList = (props) => {
+    // If there is a response passed as a prop, render the list of exams
     if (props.resp) {
         let resp = props.resp;
         return (
@@ -31,23 +32,24 @@ const ExamList = (props) => {
             </>
         );
     }
+    // If there is no response yet, render a "Loading..." message
     return <p>Loading...</p>
 }
 
 function App() {
+    // Use the useApi hook to fetch the list of exams
     const { response } = useApi({ path: 'exams' });
 
     return (
         <div className="App">
+            {/* Set up the router */}
             <RouterProvider router={router} />
             <header className="App-header">
+                {/* Render the ExamList component, passing in the response as a prop */}
                 <ExamList resp={response} />
             </header>
-
         </div>
-
     );
 }
 
 export default App;
-
--- client/src/components/Exams.js
+++ client/src/components/Exams.js
@@ -1,7 +1,5 @@
 import React, { useState, useMemo } from 'react';
-
 import { useTable } from 'react-table';
-
 import { ExamDetails, PatientDetails, Search } from '../subComponent';
 import { Columns } from '../data/columns';
 import fakeData from '../data/data.json';
@@ -9,6 +7,7 @@ import fakeData from '../data/data.json';
 const Exams = () => {
     const [isPatientInfo, setIsPatientInfo] = useState(false);
     const [isExamInfo, setIsExamInfo] = useState(false);
+    const [selectedRow, setSelectedRow] = useState(null); // Track selected row
     const columns = useMemo(() => Columns, []);
     const data = useMemo(() => fakeData, []);
 
@@ -21,9 +20,15 @@ const Exams = () => {
         prepareRow,
     } = dataTable;
 
+    // Function to handle row click
+    const handleRowClick = (row) => {
+        setSelectedRow(row); // Set selected row to the clicked row
+        setIsExamInfo(true); // Display ExamDetails component
+    };
+
     return (
         <>
-            {!(isExamInfo || isPatientInfo) &&
+            {!(isExamInfo || isPatientInfo) && (
                 <div>
                     <div>
                         <h1> Exam page</h1>
@@ -31,38 +36,60 @@ const Exams = () => {
                     </div>
                     <div>
                         <table {...getTableProps()} className='tableH'>
-                            <thead >
+                            <thead>
                                 {headerGroups.map((hg) => (
                                     <tr {...hg.getHeaderGroupProps()}>
-                                        {
-                                            hg.headers.map((column) => (
-                                                <th {...column.getHeaderProps()}> {column.render('Header')} </th>))}
-                                    </tr>))
-                                }
+                                        {hg.headers.map((column) => (
+                                            <th {...column.getHeaderProps()}>
+                                                {column.render('Header')}
+                                            </th>
+                                        ))}
+                                    </tr>
+                                ))}
                             </thead>
-                            <tbody {...getTableBodyProps()} >
-                                {
-                                    rows.map(row => {
-                                        prepareRow(row)
-                                        return (
-                                            <tr {...row.getRowProps()}>
-                                                {
-                                                    row.cells.map((cell, id) => {
-                                                        return <td {...cell.getCellProps()}>{cell.render('Cell')} </td>
-                                                    })
-                                                }
-                                            </tr>)
-                                    })
-                                }
+                            <tbody {...getTableBodyProps()}>
+                                {rows.map(row => {
+                                    prepareRow(row)
+                                    return (
+                                        <tr {...row.getRowProps()} onClick={() => handleRowClick(row)}>
+                                            {row.cells.map((cell, id) => {
+                                                return (
+                                                    <td {...cell.getCellProps()}>
+                                                        {cell.column.id === "image" ? (
+                                                            <img src={cell.value} alt="Photo" width="50" height="50" />
+                                                        ) : (
+                                                            cell.render('Cell')
+                                                        )}
+                                                    </td>
+                                                )
+                                            })}
+                                        </tr>
+                                    )
+                                })}
                             </tbody>
                         </table>
                     </div>
-
-                </div>}
-            {isExamInfo && <ExamDetails />}
+                </div>
+            )}
+            {isExamInfo && selectedRow && (
+                <div>
+                    <button onClick={() => setIsExamInfo(false)}>Back</button>
+                    <div>
+                        <h2>Patient ID: {selectedRow.original.patient_id}</h2>
+                        <h2>Exam ID: {selectedRow.original.exam_id}</h2>
+                        <img src={selectedRow.original.image} alt="Photo" width="50" height="50" />
+                        <h2>Key Findings: {selectedRow.original.key_findings}</h2>
+                        <h2>Brixia Score: {selectedRow.original.brixia_score}</h2>
+                        <h2>Age: {selectedRow.original.age}</h2>
+                        <h2>Sex: {selectedRow.original.sex}</h2>
+                        <h2>BMI: {selectedRow.original.bmi}</h2>
+                        <h2>Zip Code: {selectedRow.original.zip_code}</h2>
+                    </div>
+                </div>
+            )}
             {isPatientInfo && <PatientDetails />}
         </>
     );
 }
 
-export default Exams
+export default Exams;
