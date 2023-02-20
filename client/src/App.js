298e3695ce934c111aeca7561be1caa9f442770c
Isabel Silva <silva.i@northeastern.edu>
2023-02-20 14:34:06 -0500

    Isabel implement Damien changes to display table based on API Calls

--- client/src/App.js
+++ client/src/App.js
@@ -1,55 +1,46 @@
-import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from "react-router-dom";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
 // Import necessary libraries and components
-import './App.css';
-import { Admin, Exams, HeaderCom } from './components';
-import { useApi } from './hooks/use-api';
+import "./App.css";
+import { Admin, Exams, HeaderCom } from "./components";
+import { useApi } from "./hooks/use-api";
 
 // Create the router using react-router-dom
 const router = createBrowserRouter(
-    createRoutesFromElements(
-        <Route path='/' element={<div><HeaderCom /> <Exams /></div>}>
-            <Route path='/admin' element={<Admin />} />
-        </Route>
-    )
-)
-
-// This component is used to render a list of exams
-const ExamList = (props) => {
-    // If there is a response passed as a prop, render the list of exams
-    if (props.resp) {
-        let resp = props.resp;
-        return (
-            <>
-                {
-                    resp.map(exam => (
-                        <div key={exam["_id"]}>
-                            <p>{exam["_id"]}</p>
-                            <p>{exam["keyFindings"]}</p>
-                            <p>{exam["brixiaScore"]}</p>
-                        </div>
-                    ))
-                }
-            </>
-        );
-    }
-    // If there is no response yet, render a "Loading..." message
-    return <p>Loading...</p>
-}
+  createRoutesFromElements(
+    <Route
+      path="/"
+      element={
+        <div>
+          <HeaderCom /> <Exams />
+        </div>
+      }
+    >
+      <Route path="/admin" element={<Admin />} />
+    </Route>
+  )
+);
 
 function App() {
-    // Use the useApi hook to fetch the list of exams
-    const { response } = useApi({ path: 'exams' });
+  // Use the useApi hook to fetch the list of exams
+  const { response } = useApi({ path: "exams" });
+  const { response2 } = useApi({ path: "patients" });
 
-    return (
-        <div className="App">
-            {/* Set up the router */}
-            <RouterProvider router={router} />
-            <header className="App-header">
-                {/* Render the ExamList component, passing in the response as a prop */}
-                <ExamList resp={response} />
-            </header>
-        </div>
-    );
+  return (
+    <div className="App">
+      {/* Set up the router */}
+      <RouterProvider router={router} />
+      <header className="App-header">
+        {/* Render the Exams component, passing in the response as a prop */}
+
+        <Exams resp={response} resp2={response2} />
+      </header>
+    </div>
+  );
 }
 
 export default App;
--- client/src/components/Exams.js
+++ client/src/components/Exams.js
@@ -1,95 +1,74 @@
-import React, { useState, useMemo } from 'react';
-import { useTable } from 'react-table';
-import { ExamDetails, PatientDetails, Search } from '../subComponent';
-import { Columns } from '../data/columns';
-import fakeData from '../data/data.json';
+import React, { useState, useMemo } from "react";
+import { ExamDetails, PatientDetails, Search } from "../subComponent";
 
-const Exams = () => {
-    const [isPatientInfo, setIsPatientInfo] = useState(false);
-    const [isExamInfo, setIsExamInfo] = useState(false);
-    const [selectedRow, setSelectedRow] = useState(null); // Track selected row
-    const columns = useMemo(() => Columns, []);
-    const data = useMemo(() => fakeData, []);
 
-    const dataTable = useTable({ columns, data });
-    const {
-        getTableProps,
-        getTableBodyProps,
-        headerGroups,
-        rows,
-        prepareRow,
-    } = dataTable;
+const Exams = (props) => {
+  const [isPatientInfo, setIsPatientInfo] = useState(false);
+  const [isExamInfo, setIsExamInfo] = useState(false);
+  const [selectedRow, setSelectedRow] = useState(null); // Track selected row
 
-    // Function to handle row click
-    const handleRowClick = (row) => {
-        setSelectedRow(row); // Set selected row to the clicked row
-        setIsExamInfo(true); // Display ExamDetails component
-    };
+  // Function to handle row click
+  const handleRowClick = (row) => {
+    setSelectedRow(row); // Set selected row to the clicked row
+    setIsExamInfo(true); // Display ExamDetails component
+    console.log(selectedRow);
+    // Go to ExamDetails Page
+  };
 
+  if (props.resp) {
+    let resp = props.resp;
     return (
-        <>
-            {!(isExamInfo || isPatientInfo) && (
-                <div>
-                    <div>
-                        <h1> Exam page</h1>
-                        <Search />
-                    </div>
-                    <div>
-                        <table {...getTableProps()} className='tableH'>
-                            <thead>
-                                {headerGroups.map((hg) => (
-                                    <tr {...hg.getHeaderGroupProps()}>
-                                        {hg.headers.map((column) => (
-                                            <th {...column.getHeaderProps()}>
-                                                {column.render('Header')}
-                                            </th>
-                                        ))}
-                                    </tr>
-                                ))}
-                            </thead>
-                            <tbody {...getTableBodyProps()}>
-                                {rows.map(row => {
-                                    prepareRow(row)
-                                    return (
-                                        <tr {...row.getRowProps()} onClick={() => handleRowClick(row)}>
-                                            {row.cells.map((cell, id) => {
-                                                return (
-                                                    <td {...cell.getCellProps()}>
-                                                        {cell.column.id === "image" ? (
-                                                            <img src={cell.value} alt="Photo" width="50" height="50" />
-                                                        ) : (
-                                                            cell.render('Cell')
-                                                        )}
-                                                    </td>
-                                                )
-                                            })}
-                                        </tr>
-                                    )
-                                })}
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            )}
-            {isExamInfo && selectedRow && (
-                <div>
-                    <button onClick={() => setIsExamInfo(false)}>Back</button>
-                    <div>
-                        <h2>Patient ID: {selectedRow.original.patient_id}</h2>
-                        <h2>Exam ID: {selectedRow.original.exam_id}</h2>
-                        <img src={selectedRow.original.image} alt="Photo" width="50" height="50" />
-                        <h2>Key Findings: {selectedRow.original.key_findings}</h2>
-                        <h2>Brixia Score: {selectedRow.original.brixia_score}</h2>
-                        <h2>Age: {selectedRow.original.age}</h2>
-                        <h2>Sex: {selectedRow.original.sex}</h2>
-                        <h2>BMI: {selectedRow.original.bmi}</h2>
-                        <h2>Zip Code: {selectedRow.original.zip_code}</h2>
-                    </div>
-                </div>
-            )}
-            {isPatientInfo && <PatientDetails />}
-        </>
+      <>
+        {!(isExamInfo || isPatientInfo) && (
+          <div>
+            <div>
+              <h1> Exam page</h1>
+              <Search />
+            </div>
+            <div>
+              <table className="tableH">
+                <thead>
+                  <tr>
+                    <th>Exam ID</th>
+                    <th>Patient ID</th>
+                    <th>Image</th>
+                    <th>Key Findings</th>
+                    <th>Brixia Score</th>
+                    <th>Age</th>
+                    <th>Sex</th>
+                    <th>BMI</th>
+                    <th>Zip Code</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {resp.map((exam) => (
+                    <tr onClick={() => handleRowClick(exam)}>
+                      {/* Somehow need to click on this to go to ExamDetails, Maybe using <Link> React component */}
+                      <td> {exam["_id"]}</td>
+                      <td> {exam["patientID"]}</td>
+                      <td>
+                        {" "}
+                        <img
+                          src={exam["image"]}
+                          alt="Photo"
+                          width="50"
+                          height="50"
+                        />{" "}
+                      </td>
+                      <td> {exam["keyFindings"]}</td>
+                      <td> {exam["brixiaScore"]}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        )}
+        {isPatientInfo && <PatientDetails />}
+      </>
     );
-}
+  }
+  return <p>Loading...</p>
+};
 
 export default Exams;
