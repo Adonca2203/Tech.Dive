6bd9064bd72902fa7352f8e1b10c73a5b1e972c1
Waliyat1 <53194072+Waliyat1@users.noreply.github.com>
2023-02-22 13:32:10 -0500

    added rowclick

--- client/src/subComponent/ExamDetails.js
+++ client/src/subComponent/ExamDetails.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+// import React, { useEffect, useState } from "react";
+// import { useParams } from "react-router-dom";
 
 
 const ExamDetails = (exam) => {
