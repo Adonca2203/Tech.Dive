d336acaae9d5dbb6efd493c4e81ab87ac7ca13fc
Kai Brown <kaibrown28@outlook.com>
2023-02-23 14:53:43 -0500

    cards work, can see props via browser inspection

--- client/src/subComponent/ExamDetails.js
+++ client/src/subComponent/ExamDetails.js
@@ -1,39 +1,36 @@
 // import { Search } from "../subComponent";
 import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
-
 import axios from 'axios';
 import { useEffect, useState } from "react";
-
 import { useApi, Methods } from '../hooks/use-api';
 
+// function getExam() {
+//   // const examAPI = useApi({ path: `exams/details/${props.examId}`})
 
-
-
-   
 const ExamDetails = (props) => {
+const examAPI = 'http://localhost:9000/exams'
+const patientAPI = 'http://localhost:9000/patients'
 
-  let [exam, setExam] = useState('')
-  // const testID = "63ddab3dc9c1d0397e4c7b08";
+const [exam, setExam] =useState([])
+const [patient, setPatient] =useState([])
 
-  const examAPI = useApi({ path: `exams/details/${props.examId}`})
+ const GetExam = () => {
+  axios.get(examAPI)
+  .then(response => setExam(response.data), (err) => console.error(err))
+  .catch((error) => console.error(error))
+ }
+ const GetPatient = () => {
+  axios.get(patientAPI)
+  .then(response => setPatient(response.data), (err) => console.error(err))
+  .catch((error) => console.error(error))
+ };
 
-  
-  // const getExam = () => {
-  //   axios
-  //     .get(response)
-  //     .then(
-  //       (res => setExam(res.data),
-  //       (err) => console.error(err)
-  //     )
-  //     .catch((error) => console.error))
-  // };
-  
-  // useEffect(() => {
-  //   getExam()
-  // }, [])
+
+ useEffect(()=> {GetExam()}, ([]))
+ useEffect(()=> {GetPatient()}, ([]))
   return (
-   
+      
     <>
       
       <Container  class="patientInfo">
@@ -41,35 +38,35 @@ const ExamDetails = (props) => {
         <Card class="PatientID" key={exam.id}>
           <Card.Body>
             <Card.Title>Patient ID</Card.Title>
-            <Card.Text>{props.patient.patientId}</Card.Text>
+            <Card.Text>{props.patientID}</Card.Text>
           </Card.Body>
         </Card>
 
         <Card class="Age">
           <Card.Body>
             <Card.Title>Age</Card.Title>
-            <Card.Text>{props.patient.age}</Card.Text>
+            <Card.Text>{props.age}</Card.Text>
           </Card.Body>
         </Card>
         
         <Card class="Sex">
           <Card.Body>
             <Card.Title>Sex</Card.Title>
-            <Card.Text>{props.patient.sex}</Card.Text>
+            <Card.Text>{props.sex}</Card.Text>
           </Card.Body>
         </Card>
         
         <Card class="BMI">
           <Card.Body>
             <Card.Title>BMI</Card.Title>
-            <Card.Text>{props.patient.bmi}</Card.Text>
+            <Card.Text>{props.bmi}</Card.Text>
           </Card.Body>
         </Card>
         
         <Card class="ZipCode">
           <Card.Body>
             <Card.Title>Zip Code</Card.Title>
-            <Card.Text>{props.patient.zipCode}</Card.Text>
+            <Card.Text>{props.zipCode}</Card.Text>
           </Card.Body>
         </Card>
       </Container>
@@ -79,7 +76,7 @@ const ExamDetails = (props) => {
         <Card class="ExamID" key={exam.id}>
           <Card.Body>
             <Card.Title>Exam ID</Card.Title>
-            <Card.Text>{props.exam.examId}</Card.Text>
+            <Card.Text>{props._id}</Card.Text>
           </Card.Body>
         </Card>
 
@@ -87,25 +84,25 @@ const ExamDetails = (props) => {
           <Card.Body>
             <Card.Title>Image URL</Card.Title>
             <Card.Text>URL:</Card.Text>
-            {/* Add an image here */}
+            Add an image here
           </Card.Body>
         </Card>
         <Card class="Date">
           <Card.Body>
             <Card.Title>Date</Card.Title>
-            <Card.Text>{props.exam.date}</Card.Text>
+            <Card.Text>{props.date}</Card.Text>
           </Card.Body>
         </Card>
         <Card class="KeyFindings">
           <Card.Body>
             <Card.Title>Key Findings</Card.Title>
-            <Card.Text>{props.exam.keyFindings}</Card.Text>
+            <Card.Text>{props.keyFindings}</Card.Text>
           </Card.Body>
         </Card>
         <Card class="BrixiaScore">
           <Card.Body>
             <Card.Title>Brixia Score (seperated by Comma)</Card.Title>
-            <Card.Text>{props.exam.brixiaScore}</Card.Text>
+            <Card.Text>{props.brixiaScore}</Card.Text>
           </Card.Body>
         </Card>
   </Container> 
