07fed12ac7efc7e4004e9bd3e2cd89257c3ada4c
Waliyat1 <53194072+Waliyat1@users.noreply.github.com>
2023-02-23 12:59:12 -0500

    New changes

--- client/src/subComponent/ExamDetails.js
+++ client/src/subComponent/ExamDetails.js
@@ -1,20 +1,15 @@
-// import React, { useEffect, useState } from "react";
-// import { useParams } from "react-router-dom";
-
+import React, {useState} from 'react';
+import PatientDetails from '../subComponent/PatientDetails';
 
 const ExamDetails = (exam) => {
-  // const { id } = useParams();
-  // const [exam, setExam] = useState({});
-
-  // useEffect(() => {
-  //   fetch(`/api/exams/${id}`)
-  //     .then(res => res.json())
-  //     .then(data => setExam(data));
-  // }, [id]);
-
+  const [isPatientInfo, setIsPatientInfo] = useState(false);
+  const[isExamInfo, setIsExamInfo] = useState(false);
+  
     return (
-        <div>
-          <h3>Exam Details</h3>
+        <>
+     { !(isExamInfo || isPatientInfo)  &&
+        <div>   
+        <h3>Exam Details</h3>
           <p>Patient ID: {exam.patientId}</p>
           <p>Exam ID: {exam.examId}</p>
           <p>Key findings: {exam.keyFindings}</p>
@@ -24,7 +19,11 @@ const ExamDetails = (exam) => {
           <p>BMI: {exam.bmi}</p>
           <p>Zip code: {exam.zipCode}</p>
           <img src={exam.imageUrl} alt="Patient's Chest X-Ray" />  
-        </div>
+        </div>  }
+            {isExamInfo && <ExamDetails/>}
+            {isPatientInfo && <PatientDetails/>}     
+    </>
+    
     );
 }
 
