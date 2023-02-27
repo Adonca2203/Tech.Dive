b28aa41593eee86ac47177314c4c1c4afeab602e
Kai Brown <kaibrown28@outlook.com>
2023-02-27 13:06:12 -0500

    trying to call examdetails props differently

--- client/src/subComponent/ExamDetails.js
+++ client/src/subComponent/ExamDetails.js
@@ -6,25 +6,30 @@ import { useEffect, useState } from "react";
 import { useApi, Methods } from '../hooks/use-api';
 
 // function getExam() {
-//   // const examAPI = useApi({ path: `exams/details/${props.examId}`})
 
-const ExamDetails = (props) => {
-const examAPI = 'http://localhost:9000/exams'
-const patientAPI = 'http://localhost:9000/patients'
-
-const [exam, setExam] =useState([])
-const [patient, setPatient] =useState([])
-
- const GetExam = () => {
-  axios.get(examAPI)
-  .then(response => setExam(response.data), (err) => console.error(err))
-  .catch((error) => console.error(error))
+const GetExam = (props) => {
+  if (props.response){
+    let response =  props.response;
+    return(
+      <>
+      {response.render( details => (
+        <div key={details["_id"]}>
+<p>{details}</p>
+                            <p>{details["keyFindings"]}</p>
+                            <p>{details["brixiaScore"]}</p>
+        </div>
+      ))}
+     
+      
+      </>
+    )
+  }
  }
- const GetPatient = () => {
-  axios.get(patientAPI)
-  .then(response => setPatient(response.data), (err) => console.error(err))
-  .catch((error) => console.error(error))
- };
+
+const ExamDetails = () => {
+
+const { details } = useApi({ path: `exams/details/`}, {method: Methods.GET})
+
 
 
 //  useEffect(()=> {GetExam()}, ([]))
@@ -32,80 +37,8 @@ const [patient, setPatient] =useState([])
   return (
       
     <>
-      
-      <Container className="patientInfo">
-        
-        <Card className="PatientID" key={exam.id}>
-          <Card.Body>
-            <Card.Title>Patient ID</Card.Title>
-            <Card.Text>{props.patientID}</Card.Text>
-          </Card.Body>
-        </Card>
-
-        <Card className="Age">
-          <Card.Body>
-            <Card.Title>Age</Card.Title>
-            <Card.Text>{props.age}</Card.Text>
-          </Card.Body>
-        </Card>
-        
-        <Card className="Sex">
-          <Card.Body>
-            <Card.Title>Sex</Card.Title>
-            <Card.Text>{props.sex}</Card.Text>
-          </Card.Body>
-        </Card>
-        
-        <Card className="BMI">
-          <Card.Body>
-            <Card.Title>BMI</Card.Title>
-            <Card.Text>{props.bmi}</Card.Text>
-          </Card.Body>
-        </Card>
-        
-        <Card className="ZipCode">
-          <Card.Body>
-            <Card.Title>Zip Code</Card.Title>
-            <Card.Text>{props.zipCode}</Card.Text>
-          </Card.Body>
-        </Card>
-      </Container>
-      
-      <Container className="examInfo">
-        
-        <Card className="ExamID" key={exam.id}>
-          <Card.Body>
-            <Card.Title>Exam ID</Card.Title>
-            <Card.Text>{props._id}</Card.Text>
-          </Card.Body>
-        </Card>
-
-        <Card className="Image">
-          <Card.Body>
-            <Card.Title>Image URL</Card.Title>
-            <Card.Text>URL:</Card.Text>
-            Add an image here
-          </Card.Body>
-        </Card>
-        <Card className="Date">
-          <Card.Body>
-            <Card.Title>Date</Card.Title>
-            <Card.Text>{props.date}</Card.Text>
-          </Card.Body>
-        </Card>
-        <Card className="KeyFindings">
-          <Card.Body>
-            <Card.Title>Key Findings</Card.Title>
-            <Card.Text>{props.keyFindings}</Card.Text>
-          </Card.Body>
-        </Card>
-        <Card className="BrixiaScore">
-          <Card.Body>
-            <Card.Title>Brixia Score (seperated by Comma)</Card.Title>
-            <Card.Text>{props.brixiaScore}</Card.Text>
-          </Card.Body>
-        </Card>
-  </Container> 
+     
+      <GetExam details={details} />
     </>
   );
 };
