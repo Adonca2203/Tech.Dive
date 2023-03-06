7039b5da26e78a70b66b49e664dfb67dbe7d1654
DDjohnson21 <Damienjking84@gmail.com>
2023-03-06 18:44:41 -0500

    edits to exam page - and test to show display

--- client/src/subComponent/ExamDetails.js
+++ client/src/subComponent/ExamDetails.js
@@ -1,9 +1,77 @@
-import React from 'react'
+import React from "react";
 
-const ExamDetails = () => {
+const ExamDetails = ({ exam }) => {
+  const tableStyle = {
+    fontSize: "24px",
+    textAlign: "left",
+    width: "100%",
+  };
+
+  const thStyle = {
+    padding: "12px",
+    borderBottom: "2px solid #ddd",
+    verticalAlign: "top",
+  };
+
+  const tdStyle = {
+    padding: "12px",
+    borderBottom: "1px solid #ddd",
+    verticalAlign: "top",
+  };
+
+  const imgStyle = {
+    width: "100%",
+    maxWidth: "400px",
+    height: "auto",
+  };
+
   return (
-    <div><h3>Exam Details</h3></div>
-  )
-}
+    <div>
+      <h1 style={{ fontSize: "36px" }}>Exam Details</h1>
+      <table style={tableStyle}>
+        <tbody>
+          <tr>
+            <th style={thStyle}>Exam ID:</th>
+            <td style={tdStyle}>{exam._id}</td>
+          </tr>
+          <tr>
+            <th style={thStyle}>Patient ID:</th>
+            <td style={tdStyle}>{exam.patientID}</td>
+          </tr>
+          <tr>
+            <th style={thStyle}>Image:</th>
+            <td style={tdStyle}>
+              <img src={exam.image} alt="Photo" style={imgStyle} />
+            </td>
+          </tr>
+          <tr>
+            <th style={thStyle}>Key Findings:</th>
+            <td style={tdStyle}>{exam.keyFindings}</td>
+          </tr>
+          <tr>
+            <th style={thStyle}>Brixia Score:</th>
+            <td style={tdStyle}>{exam.brixiaScore}</td>
+          </tr>
+          <tr>
+            <th style={thStyle}>Age:</th>
+            <td style={tdStyle}>{exam.age}</td>
+          </tr>
+          <tr>
+            <th style={thStyle}>Sex:</th>
+            <td style={tdStyle}>{exam.sex}</td>
+          </tr>
+          <tr>
+            <th style={thStyle}>BMI:</th>
+            <td style={tdStyle}>{exam.bmi}</td>
+          </tr>
+          <tr>
+            <th style={thStyle}>Zip Code:</th>
+            <td style={tdStyle}>{exam.zipCode}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 export default ExamDetails;
